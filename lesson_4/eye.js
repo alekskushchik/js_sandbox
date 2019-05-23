1dc5dca791f2d03a702b67c17e112cf2303184ea
alekskushchik <40305473+alekskushchik@users.noreply.github.com>
2019-05-23 21:34:07 +0300

    Update eye.js

--- lesson_4/eye.js
+++ lesson_4/eye.js
@@ -1,1 +1,23 @@
+const eye1 = document.getElementById('left-eye');
+const eye2 = document.getElementById('right-eye');
 
+
+let x1 = screenX + 35, y1 = screenY + 25;
+let r = 5, x , y, x2, y2, isEyeProcessed = false;
+
+window.addEventListener('mousemove', e => {
+    if (!isEyeProcessed) {
+        isEyeProcessed = true;
+        let x2 = e.clientX, y2 = e.clientY;
+        y = ((r * (y2 - y1)) / Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1))) + y1;
+        x = (((y - y1) * (x2 - x1)) / (y2 - y1)) + x1;
+        
+        eye1.style.marginTop = `${(y - y1 + 2)}px`;
+        eye1.style.marginLeft = `${(x - x1)}px`;
+        
+        eye2.style.marginTop = `${(y - y1 + 2)}px`;
+        eye2.style.marginLeft = `${(x - x1)}px`;
+        
+        isEyeProcessed = false;
+    }
+});
