f611b328a927f159fb9a40c25a4ba673e1888c2b
Aleksandr Kushchik <alkushchik@gmail.com>
2019-07-09 06:04:07 +0300

    Update index.jsx

--- lesson_14/index.jsx
+++ lesson_14/index.jsx
@@ -1,14 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import WrapInEmoji from './emoji';
 
+export default class WrapInEmoji extends React.Component {
+    constructor() {
+        super();
 
+        this.state = {
+            emoji: String.fromCodePoint(0x1F6 + Math.floor(Math.random() * 39 + 1))
+        };
+        
+        this.state.default = true;
+    }
 
-
-    ReactDOM.render((
-        <div>
-            <WrapInEmoji />
-            Some text
-            <WrapInEmoji />
-        </div>
-    ), document.getElementById('root'));
+    onEmojiClick() {
+        this.setState({
+            emoji: String.fromCodePoint(0x1F601 + Math.round(Math.random() * 39 + 1)),
+        });
+    }
+    
+    render() {
+        if(this.props.defaultEmoji && this.state.default) {
+            this.state = {
+                emoji: this.props.defaultEmoji
+            };
+        }
+        return (
+            <div>
+                <span onClick={this.onEmojiClick.bind(this)}>{this.state.emoji}</span>
+                {this.props.children}
+                <span onClick={this.onEmojiClick.bind(this)}>{this.state.emoji}</span>
+            </div>
+        );
+    }
+}
