0dca7b1aa32dd38bcdbcc9034c64690ff03a2b62
Aleksandr Kushchik <alkushchik@gmail.com>
2019-07-09 06:28:47 +0300

    Create countries.jsx

---
+++ lesson_14/countries.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+
+export default class CountrySelect extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            country: [
+                {}
+            ],
+            item: 0,
+        };
+        this.getCountriesList();
+    }
+    getCountriesList() {
+        fetch('https://restcountries.eu/rest/v2/all',{
+            method: 'GET',
+        })
+            .then(response => response.json())
+            .then(response => {
+
+                this.setState({
+                    country: response,
+                });
+                this.loadList();
+            });
+    }
+    loadList(){
+        let result = [];
+        for (let i = 0; i < this.state.country.length; i += 1){
+
+            result.push(
+                <li key={i}>
+                    <div>
+                        <img src={this.state.country[i]['flag']}></img>
+                    </div>
+                    {this.state.country[i]['name']}<br/>
+                    Capital: {this.state.country[i]['capital']}
+                </li>)
+        }
+        return result;
+    }
+    render() {
+        return (
+            <div>
+                <ul>
+                    {this.loadList()}
+                </ul>
+            </div>
+        );
+    }
+}
